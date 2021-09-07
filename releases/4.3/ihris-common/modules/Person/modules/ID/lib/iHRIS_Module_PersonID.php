db23ce91451cbdedb386cee993c1751b0949c4bb
Agaba Andrew <agabaandre@gmail.com>
2021-09-07 21:06:04 +0300

    add

--- releases/4.3/ihris-common/modules/Person/modules/ID/lib/iHRIS_Module_PersonID.php
+++ releases/4.3/ihris-common/modules/Person/modules/ID/lib/iHRIS_Module_PersonID.php
@@ -170,10 +170,25 @@ class iHRIS_Module_PersonID extends I2CE_Module {
                 $form->setInvalidMessage('expiration_date','bad_date');
             }
         }*/
+       
         
         /*Validate ID Pattern, this is optional*/
         $id_number = $form->getField('id_num')->getDBValue();
         $id_type = $form->getField('id_type')->getDBValue();
+
+        if ($id_type=='id_type|1'){
+            if (!is_numeric($id_number)){
+                $form->setInvalidMessage('id_num', 'Invalid National Card Number');
+            }
+
+        }
+        if ($id_type=='id_type|national_id'){
+            if (is_numeric($id_number)){
+                $form->setInvalidMessage('id_num', 'Invalid National ID Number');
+            }
+
+        }
+
         if ($id_type != ""){
             $formObj = I2CE_FormFactory::instance()->createContainer($id_type);
             if ($formObj instanceof I2CE_Form) {
