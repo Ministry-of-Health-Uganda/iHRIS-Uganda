438b78e567868e55a890646af8f2c1db770625b6
Agaba Andrew <agabaandre@gmail.com>
2021-11-06 12:42:48 +0300

    update attendance

--- releases/4.3/sites/national/update_attendance.php
+++ releases/4.3/sites/national/update_attendance.php
@@ -47,88 +47,86 @@ echo "Execution Time: " . ini_get( "max_execution_time" ) . "\n";
     //  if (!isset($person_attendance_id)){
     //     echo "No Ids \n";
     //   }else{
-    //             $count = 0;
+                $count = 0;
     //     foreach ( $person_attendance_id as $id=>$value  ){                //echo 'person_attendance'.'|'.$key ;
            
 		        $person_attendance = $form_factory->createContainer( 'person_attendance|634036');
 		        $person_attendance->populate();
 
            // print_r($person_attendance->days_leave);
-           print_r($person_attendance->month_year);
+          // print_r($person_attendance->month_year);
 
 
 			
-    // if(isset($person_attendance->days_present)||isset($person_attendance->days_or)||isset($person_attendance->days_od)||isset($person_attendance->days_leave)){
+    if(isset($person_attendance->days_present)||isset($person_attendance->days_or)||isset($person_attendance->days_od)||isset($person_attendance->days_leave)){
 
-     //if ( $person_attendance->month_year->isValid() ) {
+     if ($person_attendance->month_year->isValid() ) {
 
-	// 	//$current_year = date('Y', strtotime('$person_attendance->month_year_day'));
+		//$current_year = date('Y', strtotime('$person_attendance->month_year_day'));
 
-  //               //$current_month = date('F', strtotime('$person_attendance->month_year_day'));
+                //$current_month = date('F', strtotime('$person_attendance->month_year_day'));
       
-	// 	$values = explode('-',$person_attendance->getField("month_year")->getDBValue());
+	  	$values = explode('-',$person_attendance->getField("month_year")->getDBValue());
 
-	// 	//I2CE::raiseError($current_year."---".$values[0]);
+		//I2CE::raiseError($current_year."---".$values[0]);
 
-	// 	//I2CE::raiseError($current_month."---".$values[1]);
+		//I2CE::raiseError($current_month."---".$values[1]);
 
-	// 	$no_of_days=cal_days_in_month(CAL_GREGORIAN,$values[1],$values[0]);
+	   	$no_of_days=cal_days_in_month(CAL_GREGORIAN,$values[1],$values[0]);
 
-	// $count++;
+	    $count++;
 	
-  //          }
+           }
         
 
 	
-  //       $totalDays = 0;
-	//     $totalDays = $person_attendance->days_present+$person_attendance->days_or+$person_attendance->days_od+$person_attendance->days_leave;
+        $totalDays = 0;
+	    $totalDays = $person_attendance->days_present+$person_attendance->days_or+$person_attendance->days_od+$person_attendance->days_leave;
 
 	
 	
       
             	
             	
-  //        if(isset($person_attendance->work_days)){
-  //         $person_attendance->days_absent = ($person_attendance->work_days - ($person_attendance->days_present + $person_attendance->days_or + $person_attendance->days_leave)) ;
-	//   if($person_attendance->days_absent < 0){
-	// 	$person_attendance->days_absent = 0;
-	//   }
-	//     $person_attendance->final_work_days = ($person_attendance->work_days - ( $person_attendance->days_or + $person_attendance->days_leave)) ;
-	//   }
+    // if(isset($person_attendance->work_days)){
+    //     $person_attendance->days_absent = ($person_attendance->work_days - ($person_attendance->days_present + $person_attendance->days_or + $person_attendance->days_leave)) ;
+	  // if($person_attendance->days_absent < 0){
+	  //   	$person_attendance->days_absent = 0;
+	  // }
+	  //    $person_attendance->final_work_days = ($person_attendance->work_days - ( $person_attendance->days_or + $person_attendance->days_leave)) ;
+	  // }
 
-	//   if(!isset($person_attendance->work_days)){
-  //          //$person_attendance->absenteeism_rate = NULL;
+	  if(!isset($person_attendance->work_days)){
+           //$person_attendance->absenteeism_rate = NULL;
+          // count($person_attendance->)
 	   
 
-	//   }else{
-  //         $person_attendance->absenteeism_rate = ($person_attendance->days_absent /( $person_attendance->work_days - ($person_attendance->days_or + $person_attendance->days_leave))*100);
-  //         //$month_year_split = explode('-',$form->getField("month_year")->getDBValue());
-  //         //$month_year_day = $month_year_split[0]."-".$month_year_split[1]."-"."01";
-	//   }
-	//   ///No of days absolutely absent
-  //         $person_attendance->absolute_days_absent = ($no_of_days - $totalDays) ;
-  //         $person_attendance->absolute_absenteeism_rate = (($person_attendance->absolute_days_absent / $no_of_days)*100);
-  //         //$month_year_split = explode('-',$form->getField("month_year")->getDBValue());
-  //         //$month_year_day = $month_year_split[0]."-".$month_year_split[1]."-"."01";
+	  }else{
+          $person_attendance->absenteeism_rate = ($person_attendance->days_absent /( $person_attendance->work_days - ($person_attendance->days_or + $person_attendance->days_leave))*100);
+          //$month_year_split = explode('-',$form->getField("month_year")->getDBValue());
+          //$month_year_day = $month_year_split[0]."-".$month_year_split[1]."-"."01";
+	  
+	  ///No of days absolutely absent
+          $person_attendance->absolute_days_absent = ($no_of_days - $totalDays) ;
+          $person_attendance->absolute_absenteeism_rate = (($person_attendance->absolute_days_absent / $no_of_days)*100);
+          //$month_year_split = explode('-',$form->getField("month_year")->getDBValue());
+          //$month_year_day = $month_year_split[0]."-".$month_year_split[1]."-"."01";
 
-	//   ///No of days not at facility
-  //         $person_attendance->days_not_at_facility = ($no_of_days - $person_attendance->days_present) ;
-  //         $person_attendance->per_days_not_at_facility = (($person_attendance->days_not_at_facility / $no_of_days)*100);
-  //         $month_year_split = explode('-',$person_attendance->getField("month_year")->getDBValue());
-  //         $month_year_day = $month_year_split[0]."-".$month_year_split[1]."-"."01";
-  //         //I2CE::raiseError(" date ".$month_year );
-	//       $person_attendance->getField("month_year_day")->setFromDB( $month_year_day );
-  //         }else{
-	// 	$person_attendance->setInvalidMessage('days_present', 'Enter a value in atleast one field');
-	// 	}
+	  ///No of days not at facility
+          $person_attendance->days_not_at_facility = ($no_of_days - $person_attendance->days_present) ;
+          $person_attendance->per_days_not_at_facility = (($person_attendance->days_not_at_facility / $no_of_days)*100);
+          $month_year_split = explode('-',$person_attendance->getField("month_year")->getDBValue());
+          $month_year_day = $month_year_split[0]."-".$month_year_split[1]."-"."01";
+          //I2CE::raiseError(" date ".$month_year );
+	      $person_attendance->getField("month_year_day")->setFromDB( $month_year_day );
+          }
+          else{
+	      	$person_attendance->setInvalidMessage('days_present', 'Enter a value in atleast one field');
+		      }
 	   
-	//    }
+	   }
 
-	// 	if(!isset($person_attendance->work_days)){
-
-	// 		$person_attendance->final_work_days = NULL;
-	 //	}
-	   // }
+	
 	    
 	
 
