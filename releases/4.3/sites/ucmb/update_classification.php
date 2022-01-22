04e21c202aaddd01b5e031d417a554c2fcefe6c6
Agaba Andrew <agabaandre@gmail.com>
2022-01-23 02:42:47 +0300

    ucmb jobs map

---
+++ releases/4.3/sites/ucmb/update_classification.php
@@ -0,0 +1,75 @@
+<?php
+
+$i2ce_site_user_access_init = null;
+$script = array_shift( $argv );
+if (file_exists(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'pages/local' . DIRECTORY_SEPARATOR . 'config.values.php')) {
+	require_once(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'pages/local' . DIRECTORY_SEPARATOR . 'config.values.php');
+} else {
+	require_once( dirname(__FILE__) . DIRECTORY_SEPARATOR . 'pages/config.values.php');
+}
+$i2ce_site_i2ce_path = "/var/lib/iHRIS/releases/4.3/i2ce";
+
+require_once ($i2ce_site_i2ce_path . DIRECTORY_SEPARATOR . 'I2CE_config.inc.php');
+
+@I2CE::initializeDSN($i2ce_site_dsn,   $i2ce_site_user_access_init,    $i2ce_site_module_config);
+
+unset($i2ce_site_user_access_init);
+unset($i2ce_site_dsn);
+unset($i2ce_site_i2ce_path);
+unset($i2ce_site_module_config);
+
+
+global $user;
+
+$user = new I2CE_User(1, false, false, false);
+//$db = MDB2::singleton();
+
+$db = I2CE::PDO();
+if (PEAR::isError($db)) {
+	die($db->getMessage());
+}
+
+$form_factory = I2CE_FormFactory::instance();
+
+echo "Memory Limit: " . ini_get( "memory_limit" ) . "\n";
+echo "Execution Time: " . ini_get( "max_execution_time" ) . "\n";
+
+        $find_id = array(
+                    'operator' => 'FIELD_LIMIT',
+                    'field' => 'job',
+                    'style' => 'null'
+                    );
+
+        $job_id = I2CE_FormStorage::listFields( "job", array('id'), $find_id );
+        
+     if (!isset($job_id)){
+        echo "No Ids \n";
+      }else{
+                $count = 0;
+                
+        foreach ( $job_id as $id=>$value  ){               //
+ 
+           
+		        $job = $form_factory->createContainer( 'job'.'|'.$id );
+		        $job->populate();
+				$classification_id = 'classification|3';
+                $job->getField('classification')->setFromDB( $classification_id);
+			    // $job->save( $user );
+			    // $job->cleanup();
+				print_r($id);
+		    	 
+
+			}
+			
+        		$count++;
+
+          
+	    }
+
+			  
+	    
+	
+
+//echo "DONE  ". $count." records have position information updated  ";
+
+?>
