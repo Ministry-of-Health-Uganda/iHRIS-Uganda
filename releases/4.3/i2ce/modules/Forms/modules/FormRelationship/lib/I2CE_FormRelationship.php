006f72bbcefe1ecc0fa85c9cdd66746e4bb40e69
Agaba Andrew <agabaandre@gmail.com>
2022-01-23 13:01:29 +0300

    revert

--- releases/4.3/i2ce/modules/Forms/modules/FormRelationship/lib/I2CE_FormRelationship.php
+++ releases/4.3/i2ce/modules/Forms/modules/FormRelationship/lib/I2CE_FormRelationship.php
@@ -1045,10 +1045,10 @@ class I2CE_FormRelationship extends I2CE_Fuzzy {
         $fieldObj = $formObj->getField($fieldName);
         if (!$fieldObj instanceof I2CE_FormField_MAP) {
            //new adds if(is_object($fieldObj)) 
-            if(is_object($fieldObj)){
+            // if(is_object($fieldObj)){
             I2CE::raiseError("Field $fieldName does not exist in $joinForm: " .get_class($fieldObj));
             return $ret;
-            }
+            // }
         }
         $sub_fields = $fieldObj->getDisplayedFields($style,false);
         if (count($sub_fields) <= 1)  {
