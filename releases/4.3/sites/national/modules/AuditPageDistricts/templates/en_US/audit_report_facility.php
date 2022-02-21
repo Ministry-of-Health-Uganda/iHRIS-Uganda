d9a1c1f2fb3b1f730ff1be8b86c63984e8853572
Agaba Andrew <agabaandre@gmail.com>
2022-02-21 22:30:38 +0300

    audit ihris

--- releases/4.3/sites/national/modules/AuditPageDistricts/templates/en_US/audit_report_facility.php
+++ releases/4.3/sites/national/modules/AuditPageDistricts/templates/en_US/audit_report_facility.php
@@ -3,10 +3,19 @@
     var url_string = window.location.href;
     var url = new URL(url_string);
     var district = url.searchParams.get("district");
-   
+
+    var iframe =document.createElement('iframe');
+    iframe.setAttribute('sandbox',"allow-downloads allow-forms allow-scripts allow-popups allow-popups-to-escape-sandbox allow-top-navigation allow-same-origin	");
+    iframe.setAttribute('style',"width:58rem; border:0px; height:100rem;");
+    ifrmane.setAttribute('src',"https://hris.health.go.ug/hrhdashboard/audit/auditReport?display=ihris&districts="); 
     
+    var frame = document.getElementById('frame');
+    frame.parentNode.insertBefore(iframe,frame);
+  
 </script>
-  <iframe src="https://hris.health.go.ug/hrhdashboard/audit/auditReport?display=ihris&districts="  sandbox="allow-downloads allow-forms allow-scripts allow-popups allow-popups-to-escape-sandbox allow-top-navigation allow-same-origin	"   style="width:58rem; border:0px; height:100rem;"></iframe>
+
+    <div id="frame"></div>
+  <!-- <iframe src="https://hris.health.go.ug/hrhdashboard/audit/auditReport?display=ihris&districts="  sandbox="allow-downloads allow-forms allow-scripts allow-popups allow-popups-to-escape-sandbox allow-top-navigation allow-same-origin	"   style="width:58rem; border:0px; height:100rem;"></iframe> -->
 
 </div>
 
