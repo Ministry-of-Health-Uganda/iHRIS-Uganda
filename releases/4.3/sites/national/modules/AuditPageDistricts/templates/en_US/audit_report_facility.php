74a66b4e31bb2ad15b425aa8abd91210ec282763
Agaba Andrew <agabaandre@gmail.com>
2022-02-21 21:54:58 +0300

    audit ihris

--- releases/4.3/sites/national/modules/AuditPageDistricts/templates/en_US/audit_report_facility.php
+++ releases/4.3/sites/national/modules/AuditPageDistricts/templates/en_US/audit_report_facility.php
@@ -1,12 +1,5 @@
 <div class="facility_block">
-  <script>
-    var url_string = window.location.href;
-    var url = new URL(url_string);
-    var district = url.searchParams.get("district");
-    
-</script>
-  <iframe src="https://hris.health.go.ug/hrhdashboard/audit/auditReport?display=ihris&districts=<?php echo $_GET['district'] ?>"  sandbox="allow-downloads allow-forms allow-scripts allow-popups allow-popups-to-escape-sandbox allow-top-navigation allow-same-origin	"   style="width:58rem; border:0px; height:100rem;"></iframe>
-
+  <iframe src="https://hris.health.go.ug/hrhdashboard/audit/auditReport?display=ihris&districts=<?php echo $_GET['district']; ?>"  sandbox="allow-downloads allow-forms allow-scripts allow-popups allow-popups-to-escape-sandbox allow-top-navigation allow-same-origin	"   style="width:58rem; border:0px; height:100rem;"></iframe>
 </div>
 
 
