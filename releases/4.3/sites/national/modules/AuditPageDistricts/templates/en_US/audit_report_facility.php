161c847085a14910a9542d304d537d700ea71e19
Agaba Andrew <agabaandre@gmail.com>
2022-02-21 23:08:49 +0300

    ignore

--- releases/4.3/sites/national/modules/AuditPageDistricts/templates/en_US/audit_report_facility.php
+++ releases/4.3/sites/national/modules/AuditPageDistricts/templates/en_US/audit_report_facility.php
@@ -14,7 +14,9 @@
 
     var iframe =document.createElement('iframe');
     iframe.setAttribute('sandbox',"allow-downloads allow-forms allow-scripts allow-popups allow-popups-to-escape-sandbox allow-top-navigation allow-same-origin	");
-    iframe.setAttribute('style',"width:58rem; border:0px; height:100rem;");
+    iframe.style.width='58rem';
+    iframe.style.border='0px';
+    iframe.style.height='100rem';
     iframe.setAttribute('src',`https://hris.health.go.ug/hrhdashboard/audit/auditReport?display=ihris&districts=${district}`); 
     
     var frame = document.getElementById('frame');
