4f2f94a216e2fa8ac1b482cba9291c64d4135221
Agaba Andrew <agabaandre@gmail.com>
2022-02-21 23:01:04 +0300

    ignore

--- releases/4.3/sites/national/modules/AuditPageDistricts/templates/en_US/audit_report_facility.php
+++ releases/4.3/sites/national/modules/AuditPageDistricts/templates/en_US/audit_report_facility.php
@@ -2,6 +2,10 @@
 <div class="frame"></div>
 
   <script>
+
+    document.addEventListener("DOMContentLoaded",
+    function(event){
+
     var url_string = window.location.href;
     var url = new URL(url_string);
     var district = url.searchParams.get("district");
@@ -12,8 +16,10 @@
     iframe.setAttribute('src',`https://hris.health.go.ug/hrhdashboard/audit/auditReport?display=ihris&districts=${district}`); 
     
     var frame = document.getElementsByClassName('frame');
-    //console.log(frame);
-    document.body.appendChild(iframe);
+    console.log(frame);
+    frame.parentNode.insertBefore(iframe,frame);
+
+    });
   
 </script>
 
