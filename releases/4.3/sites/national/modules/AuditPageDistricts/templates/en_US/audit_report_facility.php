6c6d22498a5265972f2b64a07ae1fc444fca4fe3
Agaba Andrew <agabaandre@gmail.com>
2022-02-21 21:50:11 +0300

    new demo

--- releases/4.3/sites/national/modules/AuditPageDistricts/templates/en_US/audit_report_facility.php
+++ releases/4.3/sites/national/modules/AuditPageDistricts/templates/en_US/audit_report_facility.php
@@ -5,7 +5,7 @@
     var district = url.searchParams.get("district");
     
 </script>
-  <iframe src="https://hris.health.go.ug/hrhdashboard/audit/auditReport?display=ihris&districts=<?php $_GET['district'] ?>"  sandbox="allow-downloads allow-forms allow-scripts allow-popups allow-popups-to-escape-sandbox allow-top-navigation allow-same-origin	"   style="width:58rem; border:0px; height:100rem;"></iframe>
+  <iframe src="https://hris.health.go.ug/hrhdashboard/audit/auditReport?display=ihris&districts=<?php echo $_GET['district'] ?>"  sandbox="allow-downloads allow-forms allow-scripts allow-popups allow-popups-to-escape-sandbox allow-top-navigation allow-same-origin	"   style="width:58rem; border:0px; height:100rem;"></iframe>
 
 </div>
 
