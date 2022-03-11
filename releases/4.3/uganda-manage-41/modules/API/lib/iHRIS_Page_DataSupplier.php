17be245d78e72788f9af9850d22a8663ebb2cbb0
Agaba Andrew <agabaandre@gmail.com>
2022-03-11 14:00:26 +0300

    cached forms

--- releases/4.3/uganda-manage-41/modules/API/lib/iHRIS_Page_DataSupplier.php
+++ releases/4.3/uganda-manage-41/modules/API/lib/iHRIS_Page_DataSupplier.php
@@ -199,8 +199,8 @@ class iHRIS_Page_DataSupplier extends I2CE_Page{
      * @param DOMNode &$top The node to append data to.
      * @return boolean
      */
-    protected function ( &$top ) {
-loadData_Practitioner
+    protected function loadData_Practitioner ( &$top ) {
+
         if(isset($this->id)){
             $person_ids = array();
             $ids = I2CE_FormStorage::listFields( 'person_id', array('parent'), false, array(
