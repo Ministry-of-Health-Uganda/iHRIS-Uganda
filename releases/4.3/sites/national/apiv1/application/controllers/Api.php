02ff3720ffe0a95e3d88f6382e0d1a30567d21b5
Agaba Andrew <agabaandre@gmail.com>
2022-04-22 23:13:24 +0300

    multi enroll

--- releases/4.3/sites/national/apiv1/application/controllers/Api.php
+++ releases/4.3/sites/national/apiv1/application/controllers/Api.php
@@ -255,7 +255,7 @@ Class Api extends REST_Controller
             "surname"=>@$result['person+surname'] ,
             "firstname"=> @$result['person+firstname'],
             "othername"=>@$result['person+othername'],
-            "gender"=>$gender,
+            "gender"=>$sex,
             "maritalStatus"=> @$this->getmarital($result['demographic+marital_status']),
             "photo" => @$image=base64_encode($this->getImagedata($result['Photo+id'])),
             "birthDate"=>@date('Y-m-d', strtotime($result['demographic+birth_date'])),
