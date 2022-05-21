eec92949ea6ba822abdf7e323b5dfb52e5f5028b
Agaba Andrew <agabaandre@gmail.com>
2022-05-21 19:41:45 +0300

    ucmb

--- releases/4.3/sites/ucmb/apiv1/application/models/Request_Model.php
+++ releases/4.3/sites/ucmb/apiv1/application/models/Request_Model.php
@@ -80,7 +80,7 @@ Class Request_Model extends CI_Model
     public function practitioner_data($offset=FALSE, $page_limit=FALSE){
 
        
-        $result = $this->db->query("SELECT * FROM `zebra_ihris_data_api` where `national_id+id_num`!='' LIMIT $offset, $page_limit")->result_array();
+        $result = $this->db->query("SELECT * FROM  zebra_staff_list where `national_id+id_num`!='' LIMIT $offset, $page_limit")->result_array();
 
     
     return $result;
