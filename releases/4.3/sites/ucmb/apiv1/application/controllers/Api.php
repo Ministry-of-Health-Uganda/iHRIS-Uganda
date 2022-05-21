68cc50c57df862fd4fd03429614d5c5d587e1790
Agaba Andrew <agabaandre@gmail.com>
2022-05-21 19:58:50 +0300

    ucmb

--- releases/4.3/sites/ucmb/apiv1/application/controllers/Api.php
+++ releases/4.3/sites/ucmb/apiv1/application/controllers/Api.php
@@ -156,7 +156,7 @@ Class Api extends REST_Controller
               "nationalID"=> array(
                 "nin"=>ucwords(str_replace(" ","",$result['national_id+id_num'])),
                 "cardNo"=> ucwords(str_replace(" ","",$result['national_id_card_no+id_num'])),
-                "expiryDate"=> $result['national_id_card_no+expiration_date']
+                "expiryDate"=> $result['national_id+expiration_date']
               ),
               "passport"=> array(
                 "passportNo"=> "",
@@ -284,7 +284,7 @@ Class Api extends REST_Controller
             "othername"=>@$result['person+othername'],
             "gender"=>$sex,
             "maritalStatus"=> @$this->getmarital($result['demographic+marital_status']),
-            "photo" => @$image=base64_encode($this->getImagedata($result['primary_form+parent'])),
+            "photo" => @$image=base64_encode($this->getImagedata($result['Photo+parent'])),
             "birthDate"=>@date('Y-m-d', strtotime($result['demographic+birth_date'])),
             "countryOfOrigin"=>@$this->getCountry($result['person+nationality']),
             "citizenship" => $citizenship,
