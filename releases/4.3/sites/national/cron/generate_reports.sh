f1e5c87a9d6797edff82c45113b69bde5c0880fe
AGABA ANDREW <agabaandre@gmail.com>
2022-10-21 21:33:46 +0300

    attandace

--- releases/4.3/sites/national/cron/generate_reports.sh
+++ releases/4.3/sites/national/cron/generate_reports.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-reports="staff_uniform structure users dutyroster_attendance search_people facility_tree position_tree disciplinary_case facility_list facility_positions former_staff person_attendance_all staff_appraisal person_appraisals leave person_mentorship person_recent_training person_training position_list registration staff_album staff_chart training_institution training_type"
+reports="staff_uniform structure users dutyroster_attendance search_people facility_tree position_tree disciplinary_case facility_list facility_positions former_staff person_attendance_all staff_appraisal person_appraisals leave person_mentorship person_recent_training person_training position_list registration staff_album staff_chart training_institution training_type Attendance_Aggregator"
 for report in $reports
 do
     echo Generating $report >> /tmp/report_cron.log
