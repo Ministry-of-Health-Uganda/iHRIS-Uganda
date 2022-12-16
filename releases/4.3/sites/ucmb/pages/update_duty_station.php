fbdb47dfcf82c3049aaea07277fe71f90b1b7ab3
Agaba Andre <agabaandre@gmail.com>
2022-12-16 12:35:44 +0300

    Update update_duty_station.php

--- releases/4.3/sites/ucmb/pages/update_duty_station.php
+++ releases/4.3/sites/ucmb/pages/update_duty_station.php
@@ -8,7 +8,7 @@ if (file_exists(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'local' . DIRECTORY_SE
 	require_once( dirname(__FILE__) . DIRECTORY_SEPARATOR . 'config.values.php');
 }
 
-$i2ce_site_i2ce_path = "/var/lib/iHRIS/releases/4.3/i2ce";
+$i2ce_site_i2ce_path = "/var/lib/iHRIS-Uganda/releases/4.3/i2ce";
 
 require_once ($i2ce_site_i2ce_path . DIRECTORY_SEPARATOR . 'I2CE_config.inc.php');
 
