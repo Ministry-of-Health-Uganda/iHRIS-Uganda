6b5fa441c132b860942e24721d3f5c9e66f576d1
Agaba Andre <agabaandre@gmail.com>
2025-01-21 23:04:29 +0300

    Update generate_reports.sh

--- releases/4.3/sites/national/cron/generate_reports.sh
+++ releases/4.3/sites/national/cron/generate_reports.sh
@@ -4,6 +4,6 @@ for report in $reports
 do
     echo Generating $report >> /tmp/report_cron.log
     date >> /tmp/report_cron.log
-    cd /var/lib/iHRIS/releases/4.3/sites/national/pages/ && php index.php --page=/CustomReports/generate_force/$report --nocheck=1
+    cd /var/lib/iHRIS-Uganda/releases/4.3/sites/national/pages/ && php index.php --page=/CustomReports/generate_force/$report --nocheck=1
     date >> /tmp/report_cron.log
 done
